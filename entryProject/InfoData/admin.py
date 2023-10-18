6c421a22f291b2a8cc7ff03c1d351358268941fd
jain07068 <59594514+jain07068@users.noreply.github.com>
2023-10-18 16:34:51 +0530

    modify

--- entryProject/InfoData/admin.py
+++ entryProject/InfoData/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from InfoData.models import InfoModal
+
+class InfoModalAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "email_address", "mobile_number", "description", "response_status"]
+
+
+admin.site.register(InfoModal, InfoModalAdmin)
