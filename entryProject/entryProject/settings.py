191f974ec0beda3fa197ecd0b1bcd30020b23693
jain07068 <59594514+jain07068@users.noreply.github.com>
2023-10-18 18:16:48 +0530

    modify

--- entryProject/entryProject/settings.py
+++ entryProject/entryProject/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = "/home/prajay/praveen/entryProject/static"
+STATIC_ROOT = "/home/prajay/praveen/entryProject/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
